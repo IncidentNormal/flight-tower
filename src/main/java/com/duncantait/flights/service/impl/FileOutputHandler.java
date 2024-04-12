18f4d05f94be35edd8b30462ad2ee0bbb586e0d8
Duncan Tait <duncan.tait@hub.com>
2024-04-13 00:17:04 +0100

    Add test harness and multiple tests

---
+++ src/main/java/com/duncantait/flights/service/impl/FileOutputHandler.java
@@ -0,0 +1,38 @@
+package com.duncantait.flights.service.impl;
+
+import com.duncantait.flights.model.FlightState;
+import com.duncantait.flights.service.OutputHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+public class FileOutputHandler implements OutputHandler {
+
+    private final String outputFilePath;
+
+    @Override
+    public void handle(List<FlightState> states) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+            for (FlightState state : states) {
+                String logLine = toLogLine(state);
+                writer.write(logLine);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            log.error("Failed to write to file: " + outputFilePath, e);
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private static String toLogLine(FlightState state) {
+        return "%s %s %d".formatted(state.getPlaneID(), state.getStatus().getVariant(), state.getLastFuelLevel());
+    }
+
+}
--- src/test/java/com/duncantait/flights/FlightsTest.java
+++ src/test/java/com/duncantait/flights/FlightsTest.java
@@ -1,19 +1,22 @@
 package com.duncantait.flights;
 
-import com.duncantait.flights.helper.LocalDateTimeHelper;
 import com.duncantait.flights.model.FlightState;
-import com.duncantait.flights.model.FlightStatus;
 import com.duncantait.flights.service.FlightEventAggregator;
 import com.duncantait.flights.service.InputHandler;
 import com.duncantait.flights.service.OutputHandler;
 import com.duncantait.flights.service.impl.FileInputHandler;
-import com.duncantait.flights.service.impl.LogOutputHandler;
+import com.duncantait.flights.service.impl.FileOutputHandler;
 import com.duncantait.flights.service.impl.SequentialFlightEventAggregator;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,87 +24,47 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Slf4j
 class FlightsTest {
 
-    // TODO big e2e test (stream lots of test data to a file, then read from file)
-
-    // Only 'create' events
-    @Test
-    void test001() {
-        var args = new String[] {"2025-01-01T00:00:00", "src/test/resources/test__001.in"};
+    @ParameterizedTest
+    @CsvSource({
+            "src/test/resources/test__001.input, 2025-01-01T00:00:00, src/test/resources/test__001.output", // only create events
+            "src/test/resources/test__001.input, 2021-03-29T11:59:00, src/test/resources/test__001_t1.output", // 001 - query time is set to 11:59
+            "src/test/resources/test__001.input, 2021-03-29T12:00:00, src/test/resources/test__001_t2.output", // 001 - query time is set to 12:00
+            "src/test/resources/test__002.input, 2025-01-01T00:00:00, src/test/resources/test__002.output", // same as 001 + an amend event
+            "src/test/resources/test__002.input, 2021-03-29T11:59:00, src/test/resources/test__002_t1.output", // 002 - query time is set to 11:59
+            "src/test/resources/test__002.input, 2021-03-29T12:00:00, src/test/resources/test__002_t2.output", // 002 - query time is set to 12:00
+            "src/test/resources/test__003.input, 2025-01-01T00:00:00, src/test/resources/test__003.output", // same as 002 + a cancel event
+            "src/test/resources/test__003.input, 2021-03-29T11:59:00, src/test/resources/test__003_t1.output", // 003 - query time is set to 11:59
+            "src/test/resources/test__003.input, 2021-03-29T12:00:00, src/test/resources/test__003_t2.output", // 003 - query time is set to 12:00
+            "src/test/resources/test__004.input, 2025-01-01T00:00:00, src/test/resources/test__004.output", // same as 003 but duplicated for another 24 hours
+            "src/test/resources/test__004.input, 2021-03-30T08:00:00, src/test/resources/test__004_t1.output", // 004 - query time is set to 08:00 on 2nd day
+            "src/test/resources/test__004.input, 2021-03-30T12:00:00, src/test/resources/test__004_t2.output", // 004 - query time is set to 12:00 on 2nd day
+    })
+    void testFlightEventProcessing(String inputFilePath, String queryTimestampStr, String expectedOutputFilePath) throws Exception {
+        LocalDateTime queryTimestamp = LocalDateTime.parse(queryTimestampStr);
+        String actualOutputFilePath = getActualOutputFilePath(inputFilePath);
 
         InputHandler inputHandler = new FileInputHandler();
         FlightEventAggregator flightEventAggregator = new SequentialFlightEventAggregator();
-        OutputHandler outputHandler = new LogOutputHandler();
+        OutputHandler outputHandler = new FileOutputHandler(actualOutputFilePath); // Use `FileOutputHandler` so we can assert the results more easily
 
         // Run code
-        LocalDateTime queryTimestamp = LocalDateTimeHelper.toLocalDateTime(args[0]);
         AtomicReference<List<FlightState>> states = new AtomicReference<>();
-        inputHandler.process(args, flightEvents -> {
+        inputHandler.process(new String[] {queryTimestampStr, inputFilePath}, flightEvents -> {
             states.set(flightEventAggregator.aggregate(flightEvents, queryTimestamp));
             outputHandler.handle(states.get());
         });
 
-        assertThat(states.get()).hasSize(4);
-        assertThat(states.get()).usingElementComparatorIgnoringFields("lastTimestamp")
-                .contains(
-                        FlightState.builder().planeID("F123").status(FlightStatus.LANDED).lastFuelLevel(104).build(),
-                        FlightState.builder().planeID("F222").status(FlightStatus.AWAITING_TAKEOFF).lastFuelLevel(200).build(),
-                        FlightState.builder().planeID("F324").status(FlightStatus.IN_FLIGHT).lastFuelLevel(0).build(),
-                        FlightState.builder().planeID("F551").status(FlightStatus.LANDED).lastFuelLevel(225).build()
-                );
+        // Read output file and assert results
+        List<String> expectedLines = Files.readAllLines(Path.of(expectedOutputFilePath));
+        List<String> actualLines = Files.readAllLines(Path.of(actualOutputFilePath));
+        assertThat(actualLines).isEqualTo(expectedLines);
     }
 
-    // Has an 'amend' event
-    @Test
-    void test002() {
-        var args = new String[] {"2025-01-01T00:00:00", "src/test/resources/test__002.in"};
-
-        InputHandler inputHandler = new FileInputHandler();
-        FlightEventAggregator flightEventAggregator = new SequentialFlightEventAggregator();
-        OutputHandler outputHandler = new LogOutputHandler();
-
-        // Run code
-        LocalDateTime queryTimestamp = LocalDateTimeHelper.toLocalDateTime(args[0]);
-        AtomicReference<List<FlightState>> states = new AtomicReference<>();
-        inputHandler.process(args, flightEvents -> {
-            states.set(flightEventAggregator.aggregate(flightEvents, queryTimestamp));
-            outputHandler.handle(states.get());
-        });
-
-        assertThat(states.get()).hasSize(4);
-        assertThat(states.get()).usingElementComparatorIgnoringFields("lastTimestamp")
-                .contains(
-                        FlightState.builder().planeID("F123").status(FlightStatus.LANDED).lastFuelLevel(104).build(),
-                        FlightState.builder().planeID("F222").status(FlightStatus.AWAITING_TAKEOFF).lastFuelLevel(200).build(),
-                        FlightState.builder().planeID("F324").status(FlightStatus.IN_FLIGHT).lastFuelLevel(0).build(),
-                        FlightState.builder().planeID("F551").status(FlightStatus.LANDED).lastFuelLevel(45).build()
-                );
-    }
-
-    // Has an 'amend' and a 'cancel' event
-    @Test
-    void test003() {
-        var args = new String[] {"2025-01-01T00:00:00", "src/test/resources/test__003.in"};
-
-        InputHandler inputHandler = new FileInputHandler();
-        FlightEventAggregator flightEventAggregator = new SequentialFlightEventAggregator();
-        OutputHandler outputHandler = new LogOutputHandler();
-
-        // Run code
-        LocalDateTime queryTimestamp = LocalDateTimeHelper.toLocalDateTime(args[0]);
-        AtomicReference<List<FlightState>> states = new AtomicReference<>();
-        inputHandler.process(args, flightEvents -> {
-            states.set(flightEventAggregator.aggregate(flightEvents, queryTimestamp));
-            outputHandler.handle(states.get());
-        });
-
-        assertThat(states.get()).hasSize(4);
-        assertThat(states.get()).usingElementComparatorIgnoringFields("lastTimestamp")
-                .contains(
-                        FlightState.builder().planeID("F123").status(FlightStatus.LANDED).lastFuelLevel(104).build(),
-                        FlightState.builder().planeID("F222").status(FlightStatus.AWAITING_TAKEOFF).lastFuelLevel(200).build(),
-                        FlightState.builder().planeID("F324").status(FlightStatus.IN_FLIGHT).lastFuelLevel(0).build(),
-                        FlightState.builder().planeID("F551").status(FlightStatus.IN_FLIGHT).lastFuelLevel(345).build()
-                );
+    private static String getActualOutputFilePath(String inputFilePath) {
+        String inputFileName = Paths.get(inputFilePath).getFileName().toString();
+        String tempDir = System.getProperty("java.io.tmpdir");
+        Path actualOutputFilePath = Paths.get(tempDir , inputFileName + "-" + UUID.randomUUID() + ".actual");
+        return actualOutputFilePath.toString();
     }
 
 }
